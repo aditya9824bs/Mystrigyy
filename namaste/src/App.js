5746c5118be80614affac79822277e93c1284b9f
aditya8924bs <adityadubey9125@gmail.com>
2024-04-11 17:02:25 +0530

    first commit

---
+++ namaste/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi react </h1>
+      <h2>Hi Mystrigyy</h2>
+      <button>click me </button>
+    </div>
+  );
+}
+
+export default App;
